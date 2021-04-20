Cadiboo/OptiFineDeobf

commit cd5a3a4a02f37821a6d33bd7148b9478ba7a91c1

Stop redistributing MCP Mappings

src/main/java/io/github/cadiboo/optifinedeobf/DeobfFrame.java
@@ -46,7 +46,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 
@@ -172,9 +171,7 @@ public class DeobfFrame extends JFrame {
 		mappingsFileLabel.setPreferredSize(new Dimension(75, 16));
 		mappingsFileLabel.setText("Mappings");
 
-		final String defaultMappings = new Random().nextBoolean() ? SRG2MCP.DEFAULT_MAPPINGS_FILE : TSRG2MCP.DEFAULT_MAPPINGS_FILE;
-
-		mappingsFileTextField = new JTextField("Default (" + defaultMappings + ")");
+		mappingsFileTextField = new JTextField("Choose Mappings...");
 		mappingsFileTextField.setName("mappingsFileTextField");
 		mappingsFileTextField.setBounds(90, 125, 210, 20);
 		mappingsFileTextField.setEditable(false);
@@ -502,17 +499,6 @@ public class DeobfFrame extends JFrame {
 		File file = Paths.get(mappingsText).toFile();
 		if (file.exists()) {
 			setMappings(file);
-		} else if (mappingsText.startsWith("Default")) {
-			int dotIndex = mappingsText.lastIndexOf('.');
-			if (dotIndex == -1)
-				handleException(new IllegalStateException("Invalid default mappings input. What? How?"));
-			String mappingsExt = mappingsText.substring(dotIndex + 1);
-			if (mappingsExt.startsWith("srg"))
-				mappingService = new SRG2MCP();
-			else if (mappingsExt.startsWith("tsrg"))
-				mappingService = new TSRG2MCP();
-			else
-				handleException(new IllegalStateException("Invalid default mappings input. What? How?"));
 		} else {
 			handleException(new IllegalStateException("Invalid mappings input"));
 		}

src/main/java/io/github/cadiboo/optifinedeobf/mapping/SRG2MCP.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
  */
 public class SRG2MCP implements MappingService {
 
-	public static final String DEFAULT_MAPPINGS_FILE = "srg_to_snapshot_20190922-1.14.3.srg";
-
 	private final HashMap<String, String> fields = new HashMap<>();
 	private final HashMap<String, String> methods = new HashMap<>();
 
@@ -41,10 +39,6 @@ public class SRG2MCP implements MappingService {
 				throw new RuntimeException("Failed parsing SRG on line " + i + " \"" + line + "\"", e);
 			}
 		}
-	}
-
-	public SRG2MCP() {
-		this(SRG2MCP.class.getResourceAsStream("/" + DEFAULT_MAPPINGS_FILE));
 	}
 
 	@Override

src/main/java/io/github/cadiboo/optifinedeobf/mapping/TSRG2MCP.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
  */
 public class TSRG2MCP implements MappingService {
 
-	public static final String DEFAULT_MAPPINGS_FILE = "srg_to_snapshot_20190922-1.14.3.tsrg";
-
 	private final HashMap<String, String> fields = new HashMap<>();
 	private final HashMap<String, String> methods = new HashMap<>();
 
@@ -37,10 +35,6 @@ public class TSRG2MCP implements MappingService {
 				throw new RuntimeException("Failed parsing TSRG on line " + i + " \"" + line + "\"", e);
 			}
 		}
-	}
-
-	public TSRG2MCP() {
-		this(TSRG2MCP.class.getResourceAsStream("/" + DEFAULT_MAPPINGS_FILE));
 	}
 
 	@Override

commit 0653584bceb020dc35d7801a5fd6545f41443721

Fix issue caused by cd5a3a4

src/test/java/io/github/cadiboo/optifinedeobf/MappingServiceTest.java
@@ -4,15 +4,42 @@ import io.github.cadiboo.optifinedeobf.mapping.MappingService;
 import io.github.cadiboo.optifinedeobf.mapping.SRG2MCP;
 import io.github.cadiboo.optifinedeobf.mapping.TSRG2MCP;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 /**
  * @author Cadiboo
  */
 public class MappingServiceTest {
 
+	// These are just small compacted samples from 'srg_to_snapshot_20190922-1.14.3'
+	private static final String TEST_SRG = "" +
+		"CL: net/minecraft/client/renderer/Quaternion net/minecraft/client/renderer/Quaternion\n" +
+		"CL: net/minecraft/client/renderer/Vector3f net/minecraft/client/renderer/Vector3f\n" +
+		"FD: net/minecraft/client/renderer/Quaternion/field_195895_a net/minecraft/client/renderer/Quaternion/components\n" +
+		"FD: net/minecraft/client/renderer/Vector3f/field_195907_a net/minecraft/client/renderer/Vector3f/components\n" +
+		"MD: net/minecraft/client/renderer/Vector3f/func_195906_d ()V net/minecraft/client/renderer/Vector3f/normalize ()V\n" +
+		"MD: net/minecraft/client/renderer/Vector3f/func_195900_b ()F net/minecraft/client/renderer/Vector3f/getY ()F\n" +
+		"";
+	private static final String TEST_TSRG = "" +
+		"net/minecraft/client/renderer/Quaternion net/minecraft/client/renderer/Quaternion\n" +
+		"\tfield_195895_a components\n" +
+		"\tequals (Ljava/lang/Object;)Z equals\n" +
+		"\tfunc_195891_b ()F getY\n" +
+		"\tfunc_195892_e ()V conjugate\n" +
+		"net/minecraft/client/renderer/Vector3f net/minecraft/client/renderer/Vector3f\n" +
+		"\tfield_195907_a components\n" +
+		"\tequals (Ljava/lang/Object;)Z equals\n" +
+		"\tfunc_195896_c (Lnet/minecraft/client/renderer/Vector3f;)V cross\n" +
+		"\tfunc_195900_b ()F getY\n" +
+		"\tfunc_214906_c (FFF)F func_214906_c\n" +
+		"\thashCode ()I hashCode\n" +
+		"";
+
 	public static void main(String[] args) {
 		final MappingService[] mappingServices = {
-				new SRG2MCP(),
-				new TSRG2MCP(),
+				new SRG2MCP(new ByteArrayInputStream(TEST_SRG.getBytes())),
+				new TSRG2MCP(new ByteArrayInputStream(TEST_TSRG.getBytes())),
 		};
 		for (MappingService service : mappingServices) {
 			System.out.println("~~~" + service.getClass().getSimpleName() + "~~~");
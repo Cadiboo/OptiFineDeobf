0653584bceb020dc35d7801a5fd6545f41443721
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2021-04-20 22:54:23 +1000

    Fix issue caused by cd5a3a4

--- src/test/java/io/github/cadiboo/optifinedeobf/MappingServiceTest.java
+++ src/test/java/io/github/cadiboo/optifinedeobf/MappingServiceTest.java
@@ -4,15 +4,42 @@ import io.github.cadiboo.optifinedeobf.mapping.MappingService;
 import io.github.cadiboo.optifinedeobf.mapping.SRG2MCP;
 import io.github.cadiboo.optifinedeobf.mapping.TSRG2MCP;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 /**
  * @author Cadiboo
  */
 public class MappingServiceTest {
 
+	// These are just small compacted samples from 'srg_to_snapshot_20190922-1.14.3'
+	private static final String TEST_SRG = "" +
+		"CL: net/minecraft/client/renderer/Quaternion net/minecraft/client/renderer/Quaternion\n" +
+		"CL: net/minecraft/client/renderer/Vector3f net/minecraft/client/renderer/Vector3f\n" +
+		"FD: net/minecraft/client/renderer/Quaternion/field_195895_a net/minecraft/client/renderer/Quaternion/components\n" +
+		"FD: net/minecraft/client/renderer/Vector3f/field_195907_a net/minecraft/client/renderer/Vector3f/components\n" +
+		"MD: net/minecraft/client/renderer/Vector3f/func_195906_d ()V net/minecraft/client/renderer/Vector3f/normalize ()V\n" +
+		"MD: net/minecraft/client/renderer/Vector3f/func_195900_b ()F net/minecraft/client/renderer/Vector3f/getY ()F\n" +
+		"";
+	private static final String TEST_TSRG = "" +
+		"net/minecraft/client/renderer/Quaternion net/minecraft/client/renderer/Quaternion\n" +
+		"\tfield_195895_a components\n" +
+		"\tequals (Ljava/lang/Object;)Z equals\n" +
+		"\tfunc_195891_b ()F getY\n" +
+		"\tfunc_195892_e ()V conjugate\n" +
+		"net/minecraft/client/renderer/Vector3f net/minecraft/client/renderer/Vector3f\n" +
+		"\tfield_195907_a components\n" +
+		"\tequals (Ljava/lang/Object;)Z equals\n" +
+		"\tfunc_195896_c (Lnet/minecraft/client/renderer/Vector3f;)V cross\n" +
+		"\tfunc_195900_b ()F getY\n" +
+		"\tfunc_214906_c (FFF)F func_214906_c\n" +
+		"\thashCode ()I hashCode\n" +
+		"";
+
 	public static void main(String[] args) {
 		final MappingService[] mappingServices = {
-				new SRG2MCP(),
-				new TSRG2MCP(),
+				new SRG2MCP(new ByteArrayInputStream(TEST_SRG.getBytes())),
+				new TSRG2MCP(new ByteArrayInputStream(TEST_TSRG.getBytes())),
 		};
 		for (MappingService service : mappingServices) {
 			System.out.println("~~~" + service.getClass().getSimpleName() + "~~~");
